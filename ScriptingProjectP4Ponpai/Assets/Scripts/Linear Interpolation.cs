a25728fe948e7bc5dbe8253d8813c8fec48f844c
SeanJ07 <91712813+SeanJ07@users.noreply.github.com>
2025-02-03 10:22:44 -0800

    a
    a

---
+++ ScriptingProjectP4Ponpai/Assets/Scripts/Linear Interpolation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+  * 
+  * 
+  / In this case, result = 4
+    float result = Mathf.Lerp (3ff, 5f, 0.5f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+        light.intensity = Math.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+    }
+    */
+}
